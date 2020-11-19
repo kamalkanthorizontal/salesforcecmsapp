951faf42f793cb90aaf1c3b69fa29c2c02ba1281
Kamalkant Gaur <kgaur@horizontal.com>
2020-11-19 15:09:48 +0530

    Added await

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ app.post('/', async (req, res, next) => {
                 console.log("Salesforce Response: ", resp);
         
                 let results = [];
-                contentType.forEach(async(ele) =>{
+                await contentType.forEach(async(ele) =>{
                     const managedContentType = ele.DeveloperName;
                     const cmsURL = `/services/data/v48.0/connect/cms/delivery/channels/${channelId}/contents/query?managedContentType=${managedContentType}&showAbsoluteUrl=true`;
                     console.log('cmsURL', cmsURL);            
